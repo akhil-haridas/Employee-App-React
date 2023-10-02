3c280dfecbe97ca81bc68b2eb13164facbe0cf5c
akhil-haridas <4khilharidas@gmail.com>
2023-10-02 19:09:35 +0530

    button style

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const AddButton = () => {
   const navigate = useNavigate()
   return (
-    <div className="flex flex-col fixed items-end md:px-10 sm:pr[2.25rem] px-[70px] mt-[-100px] w-full">
+    <div className="flex flex-col fixed items-end md:px-10 sm:pr[2.25rem] px-[70px] mt-[-150px] w-full">
       <button onClick={()=>navigate('/create')} className="hover:rotate-90 transition-transform duration-500 flex h-[50px] items-center justify-center w-[50px] rounded-full bg-blue-500 p-4">
         <img className="h-[18px]" src="images/img_plus.svg" alt="plus" />
       </button>
